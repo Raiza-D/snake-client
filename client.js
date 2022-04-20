c4337a303218a40140f0a55ddf3eab2c704d1aaa
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-19 19:58:35 -0700

    Re-factored host, port and setEncoding by adding them as values into variables within constants.js file.

--- client.js
+++ client.js
@@ -1,14 +1,15 @@
 const net = require("net");
+const { IP, Port, UTF } = require("./constants");
 
 // establishes a connection with the game server
 const connect = function() {
   const conn = net.createConnection({
-    host: "10.0.0.95",
-    port: 50541,
+    host: IP,
+    port: Port
   });
 
   // interpret incoming data as text
-  conn.setEncoding("utf8");
+  conn.setEncoding(UTF);
 
   conn.on("connect", () => {
     console.log("Successfully connected to game server!");

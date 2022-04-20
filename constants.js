28612fd30631a7b84fcd6869c8c8b811730e9730
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-20 15:09:08 -0700

    Code review with mentor Gary. Implemented suggested changes. Changes re-factored my code so that actions to change direction of snake and also keys for the special messags are all kept within one object. Commented out my old code. Will clean up later.

--- constants.js
+++ constants.js
@@ -3,25 +3,37 @@ const IP = "10.0.0.95";
 const Port = 50541;
 
 // Store keyboard input to move snake:
-const moveUp = "w";
-const moveDown = "s";
-const moveLeft = "a";
-const moveRight = "d";
+// const moveUp = "w";
+// const moveDown = "s";
+// const moveLeft = "a";
+// const moveRight = "d";
 
 // Key mappings for messages:
-const specialPlayerMsgs = {
-  y: "Go, snake, go!",
-  h: "FEED ME!!",
-  j: "Mmm pie-thon!",
-  u: "Hisssterical"
-};
+// const specialPlayerMsgs = {
+//   y: "Go, snake, go!",
+//   h: "FEED ME!!",
+//   j: "Mmm pie-thon!",
+//   u: "Hisssterical"
+// };
+
+  const actions = {
+    w: "Move: up",
+    s: "Move: down",
+    a: "Move: left",
+    d: "Move: right",
+    y: "Say: Got, snake, go!",
+    h: "Say: FEED ME!!",
+    j: "Say: Mmm pie-thon!",
+    u: "Say: Hisssterical",
+  };
 
 module.exports = {
   IP,
   Port,
-  moveUp,
-  moveDown,
-  moveLeft,
-  moveRight,
-  specialPlayerMsgs
+  // moveUp,
+  // moveDown,
+  // moveLeft,
+  // moveRight,
+  // specialPlayerMsgs
+  actions
 };

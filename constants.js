9eeef49b00473c0cfe13795fe6627c3343ae0e4b
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-19 19:59:16 -0700

    Added variables to contain host/IP address values, port value, and utf and added module exports to export variables into client.js

---
+++ constants.js
@@ -0,0 +1,16 @@
+
+const IP = "10.0.0.95";
+
+const Port = 50541;
+
+const UTF = "utf8";
+
+
+module.exports = {
+  IP,
+  Port,
+  UTF
+};
+
+// Single-source of truth -- Keeping important code into one file for easier
+// updating later on. Future maintainability.

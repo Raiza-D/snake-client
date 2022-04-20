7165dc5692753a46520666cda73662cb4df98331
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-20 15:21:59 -0700

    Deleted my old commented out code. Kept code re-factoring suggestions provided by mentor Gary. Added comment to explain actions object.

--- constants.js
+++ constants.js
@@ -2,26 +2,14 @@
 const IP = "10.0.0.95";
 const Port = 50541;
 
-// Store keyboard input to move snake:
-// const moveUp = "w";
-// const moveDown = "s";
-// const moveLeft = "a";
-// const moveRight = "d";
-
-// Key mappings for messages:
-// const specialPlayerMsgs = {
-//   y: "Go, snake, go!",
-//   h: "FEED ME!!",
-//   j: "Mmm pie-thon!",
-//   u: "Hisssterical"
-// };
-
+/* Key mappings for snake movements and special player messages.
+Letters represent keyboard stroke. */
   const actions = {
     w: "Move: up",
     s: "Move: down",
     a: "Move: left",
     d: "Move: right",
-    y: "Say: Got, snake, go!",
+    y: "Say: Go, snake, go!",
     h: "Say: FEED ME!!",
     j: "Say: Mmm pie-thon!",
     u: "Say: Hisssterical",
@@ -30,10 +18,5 @@ const Port = 50541;
 module.exports = {
   IP,
   Port,
-  // moveUp,
-  // moveDown,
-  // moveLeft,
-  // moveRight,
-  // specialPlayerMsgs
   actions
 };

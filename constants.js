75e36d0eedc5e1979b40082ad66131381390c39d
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-19 20:35:58 -0700

    Added additional lines of (commented) code written during session with a mentor. We were trying out different things and code was to supplement examples during conversation. Will clean up file later.

--- constants.js
+++ constants.js
@@ -5,12 +5,22 @@ const Port = 50541;
 
 const UTF = "utf8";
 
+// const setEncodingUTF8 = function(objParam, encoding) {
+//   objParam.setEncoding(encoding);
+// };
+
+const setEncoding = function (objParam, encoding) {
+  objParam.setEncoding(encoding);
+};
 
 module.exports = {
   IP,
   Port,
-  UTF
+  UTF,
+  // setEncodingUTF8
+  setEncoding
 };
 
 // Single-source of truth -- Keeping important code into one file for easier
 // updating later on. Future maintainability.
+// Functions can be factored too. Function names matter - verb and a noun or another verb.

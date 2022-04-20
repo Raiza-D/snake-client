03bb1b66c36f95fec18e013fe592164cb597fbaa
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-20 15:04:09 -0700

    Removed console.log with 'Connecting...' string. Moved to client.js file. As per mentor Gary, better to place it within client.js file. You want the message to print when you invoke the connect function. Message is related to connect function.

--- play.js
+++ play.js
@@ -1,9 +1,6 @@
 const { connect } = require("./client");
 const { setupInput } = require("./input");
 
-// Print message onto user's Terminal, when client-server connection initated
-console.log("Connecting...");
-
 // Store results of connect function (an object)
 const conn = connect();
 

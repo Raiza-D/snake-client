1ca6b5b466543d2ccadc2ff36d2bc6fbc9b0176c
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-20 13:45:34 -0700

    Added comments to explain Lines 5, 8, and 10. Removed line space between const connect and const setupInput.

--- play.js
+++ play.js
@@ -1,8 +1,11 @@
 const { connect } = require("./client");
-
 const { setupInput } = require("./input");
 
+// Print message onto user's Terminal, when client-server connection initated
 console.log("Connecting...");
+
+// Store results of connect function (an object)
 const conn = connect();
 
+// Pass in results from above into setupInput
 setupInput(conn);

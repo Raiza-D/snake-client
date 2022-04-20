4fc9a5d1fac1c7eb253dfb3436aed5d7888f3b5e
Raiza De Guzman <santos_dg7@hotmail.com>
2022-04-19 21:45:28 -0700

    Added variables that contained strings for key commands to move snake in all four directions. Referenced variable names within module exports object in order to export them into input.js

--- constants.js
+++ constants.js
@@ -1,26 +1,33 @@
-
+// constants.js
 const IP = "10.0.0.95";
 
 const Port = 50541;
 
-const UTF = "utf8";
+const moveUp = "w";
+const moveDown = "s";
+const moveLeft = "a";
+const moveRight = "d";
 
-// const setEncodingUTF8 = function(objParam, encoding) {
-//   objParam.setEncoding(encoding);
-// };
-
-const setEncoding = function (objParam, encoding) {
-  objParam.setEncoding(encoding);
+const specialPlayerMsgs = {
+  y: "Go, snake, go!",
+  h: "FEED ME!!",
+  j: "Mmm pie-thon!",
+  u: "Hisssterical"
 };
 
 module.exports = {
   IP,
   Port,
-  UTF,
-  // setEncodingUTF8
-  setEncoding
+  moveUp,
+  moveDown,
+  moveLeft,
+  moveRight,
+  specialPlayerMsgs
 };
 
-// Single-source of truth -- Keeping important code into one file for easier
-// updating later on. Future maintainability.
-// Functions can be factored too. Function names matter - verb and a noun or another verb.
+/* Notes from mentor session: 
+Single-source of truth -- Keeping important code into one file for easier
+updating later on. Future maintainability.
+Functions can be factored too.
+Function names matter - verb and a noun or another verb.
+*/
